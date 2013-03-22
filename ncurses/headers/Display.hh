e712da655b39f5936d5a90bd7a9ee1d775104f67
maxime sauvage <sauvag_m@pc-sauvag_m.(none)>
2013-03-22 11:06:47 +0100

    ncurses

---
+++ ncurses/headers/Display.hh
@@ -0,0 +1,36 @@
+#ifndef		__DISPLAY_HH__
+#define		__DISPLAY_HH__
+
+#include	<list>
+#include	<cstdlib>
+#include	<string>
+#include	<ncurses.h>
+#include	<unistd.h>
+#include	<iostream>
+#include	"../../headers/Game.hh"
+#include	"../../ILib.hh"
+
+class		Display :	public ILib
+{
+private:
+  char				_direction;
+  int				_maxheight;
+  int				_maxwidth;
+  char				_partchar;
+
+public:
+  Display();
+  ~Display();
+  virtual bool			Window() const;  
+  virtual void			Play(std::list<ISnake *> &sList, std::list<IFood *> &fList);
+  void				drawWall() const;
+  void				dispFood(std::list<IFood *> list) const;
+  void				displayOver();
+  void				displayPoints();
+  void				movesnake(std::list<ISnake *> &list, std::list<IFood *> &fList);
+  void				start();
+};
+
+typedef Display *(*maker_Display)();
+
+#endif
